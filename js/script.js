24dd96b20941dccb59ba03be7ed44f561748ba55
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-23 15:05:19 +0200

    add first list and more style details

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const app = new Vue(
+    {
+        el : '#app',
+        data : {
+            todoList : [
+                'Fare la spesa',
+                'Pulire casa',
+                'Dare da mangiare agli animali',
+                'Dare da bere ai fiori'
+            ]
+        }
+    }
+);
